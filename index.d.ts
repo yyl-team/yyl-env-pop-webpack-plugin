80295071919f4876c8b18a6f715d57a5eaae5424
liudaojie <liudaojie@yy.com>
2020-03-19 12:35:55 +0800

    feat: 补充d.ts

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,11 @@ declare class YylEnvPopWebpackPlugin {
   constructor(op: YylEnvPopWebpackPluginOptions)
 }
 interface YylEnvPopWebpackPluginOptions {
+  /** 是否启用 */
   enable?: boolean
+  /** 显示文本 */
   text?: string
+  /** 显示时间 */
+  duration?: number
 }
 export = YylEnvPopWebpackPlugin 
