3bdaaaaced633e1a71b6c42581d54eafd845c29e
liudaojie <liudaojie@yy.com>
2020-03-25 16:24:27 +0800

    feat: 兼容同时出现多个情况

--- client/client.js
+++ client/client.js
@@ -3,27 +3,29 @@
     var env = process.env.__YYL_ENV_POP__
     var tipsId = '__YYL_ENV_POP'
     var tipsEl = document.getElementById(tipsId)
-    if (!tipsEl) {
-      tipsEl = document.createElement('div')
-      tipsEl.style.cssText = [
-        'visibility: hidden',
-        'position: fixed',
-        'z-index: 10000',
-        'top: 0px',
-        'left: 50%',
-        'color: #fff',
-        'background: rgba(0, 0, 0, .6)',
-        'line-height: 2',
-        'font-size: 21px',
-        'padding: 0 10px',
-        'display: inline-block',
-        'white-space: norwap',
-        'transition: 0.5s',
-        'transform: translate(-50%, -110%)',
-        'border-radius: 0px 0px 5px 5px'
-      ].join(';')
-      document.body.appendChild(tipsEl)
+    if (tipsEl) {
+      return
     }
+    tipsEl = document.createElement('div')
+    tipsEl.id = tipsId
+    tipsEl.style.cssText = [
+      'visibility: hidden',
+      'position: fixed',
+      'z-index: 10000',
+      'top: 0px',
+      'left: 50%',
+      'color: #fff',
+      'background: rgba(0, 0, 0, .6)',
+      'line-height: 2',
+      'font-size: 21px',
+      'padding: 0 10px',
+      'display: inline-block',
+      'white-space: norwap',
+      'transition: 0.5s',
+      'transform: translate(-50%, -110%)',
+      'border-radius: 0px 0px 5px 5px'
+    ].join(';')
+    document.body.appendChild(tipsEl)
     tipsEl.innerHTML = env.text
     tipsEl.style.display = 'inline-block'
 
